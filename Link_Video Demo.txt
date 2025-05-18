68ddc4c1d665df0f5361c3e2577b661bb2210f95
exuviadot <79733846+exuviadot@users.noreply.github.com>
2025-05-18 14:51:29 +0700

    Add files via upload

---
+++ Link_Video Demo.txt
@@ -0,0 +1,1 @@
+https://youtu.be/io2jrKPca24?si=zvSnGjO277ta7qsA
---
+++ data_buku.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2025 at 05:13 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_buku`
+--
+
+CREATE TABLE `data_buku` (
+  `id_buku` int(8) NOT NULL,
+  `judul_buku` varchar(60) NOT NULL,
+  `genre_buku` varchar(60) NOT NULL,
+  `penulis_buku` varchar(60) NOT NULL,
+  `penerbit_buku` varchar(60) NOT NULL,
+  `lokasi_buku` varchar(60) NOT NULL,
+  `stok_buku` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `data_buku`
+--
+
+INSERT INTO `data_buku` (`id_buku`, `judul_buku`, `genre_buku`, `penulis_buku`, `penerbit_buku`, `lokasi_buku`, `stok_buku`) VALUES
+(8, '+1000 Aura', 'Sigma', 'Pemnulis', 'Pemnerbit', 'Lt.2', 20),
+(9, 'JudulDiubah', 'GenreUbahJg', 'PenulisUbahKali', 'PenerbitIyh', 'Disana', 15);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_buku`
+--
+ALTER TABLE `data_buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_buku`
+--
+ALTER TABLE `data_buku`
+  MODIFY `id_buku` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
